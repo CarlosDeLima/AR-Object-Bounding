62036a3235e6aebbe0cb9ebb340c87f7c73549d0
CarlosDeLima <56596108+CarlosDeLima@users.noreply.github.com>
2020-03-17 13:48:52 +0000

    First Test
    Tested by adding the "ARTapToScan" script allowing you place "Game Pieces". Script was added the the "Placement Manager" Game Object. Also, needed to add a Raycast Manager to the ARSession Origin to access the raycast options.

---
+++ Bounding Box/Assets/Scripts/ARTapToScan.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+
+
+public class ARTapToScan : MonoBehaviour
+{
+    public GameObject objectToPlace;
+    private Pose placementPose;
+    private ARRaycastManager raycastManager;
+    private bool placementPoseIsValid = false;
+    void Start()
+    {      
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+   
+    void Update()
+    {
+        UpdatePlacement();
+        if (placementPoseIsValid && Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+    
+    private void UpdatePlacement()
+    {
+        var screenCentre = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenCentre, hits, UnityEngine.XR.ARSubsystems.TrackableType.FeaturePoint);
+
+        placementPoseIsValid = hits.Count > 0;
+        if(placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
+    }
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+}
